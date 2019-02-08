7e835365d01737fa910c8a3f4883b110c52d04bc
Warren Harrison <WarrenHarrison@users.noreply.github.com>
2019-02-07 20:31:56 -0800

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,7 @@
+function counterPlus() {
+   userInterface.counter.value = eval(userInterface.counter.value) + 1;
+}
+
+function counterMinus() {
+   userInterface.counter.value = eval(userInterface.counter.value) - 1;
+}
